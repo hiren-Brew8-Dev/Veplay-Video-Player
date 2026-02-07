cedc5c03f76d3da523750fe2171fbb7c13fe0e49
hiren-Brew8-Dev <hiren.brew8.team@gmail.com>
2026-02-07 14:42:33 +0530

    Fix typo: change 'falsex' to 'false' in PlayerControlsView

--- My-Video-Player/Views/Player/PlayerControlsView.swift
+++ My-Video-Player/Views/Player/PlayerControlsView.swift
@@ -258,7 +258,7 @@ struct PlayerControlsView: View {
                             onMenu: {
                                 withAnimation(.easeInOut(duration: 0.3)) {
                                     showSettingsSheet = true
-                                    viewModel.isControlsVisible = falsex
+                                    viewModel.isControlsVisible = false
                                 }
                             },
                             showBookmarkControls: showBookmarkButton && showFloatingBookmarkControls,
